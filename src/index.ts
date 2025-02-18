8fa48387b264287c39caae8772c57c12ce3c8ca5
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-02-18 06:47:31 +0100

    chore: handlers description

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,13 @@ import { doGet } from "./handlers/httpHandler";
 import { onEdit } from "./handlers/editHandler";
 import { freshCleanup } from "./handlers/cleanupHandler";
 
-export { onFormSubmit, doGet, onEdit, freshCleanup };
+export {
+  // send emails after form submit
+  onFormSubmit,
+  // HTTP GET request handler - superior confirmation
+  doGet,
+  // Google Sheets onEdit handler - user creation
+  onEdit,
+  // CRON job - remove non-activated users
+  freshCleanup,
+};
