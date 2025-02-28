080d310888b48cf3b95a2b3d61e0bb8a6f8062d2
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-02-28 11:27:25 +0100

    feat: simplify form handler

--- src/handlers/formHandler.ts
+++ src/handlers/formHandler.ts
@@ -1,7 +1,7 @@
 import { getSheet, getRow, updateRow } from "../lib/sheet";
 import { proposeEmail } from "../lib/utils";
 import { sendEmail } from "../lib/utils";
-import { ADMIN_MAIL, PROXY_URL } from "../config";
+import { PROXY_URL } from "../config";
 
 /**
  * Handles the event
@@ -9,44 +9,32 @@ import { ADMIN_MAIL, PROXY_URL } from "../config";
 export function onFormSubmit(e: GoogleAppsScript.Events.SheetsOnFormSubmit) {
   const row = e.range.getRow();
   const sheet = getSheet();
-  const { superiorEmail, name, surname } = getRow(sheet, row);
-  const primaryEmail = `${proposeEmail(name, surname)}@zhr.pl`;
-  const options = handleFormSubmit(name, surname, primaryEmail, superiorEmail);
-
+  let { name, surname, troupName, superiorEmail, primaryEmail } = getRow(
+    sheet,
+    row
+  );
+  if (troupName) {
+    notifySuperior(superiorEmail, troupName, "", primaryEmail);
+  } else {
+    primaryEmail = `${proposeEmail(name, surname)}@zhr.pl`;
+    notifySuperior(superiorEmail, name, surname, primaryEmail);
+  }
   updateRow(sheet, row, {
-    ...options,
+    status: "Oczekiwanie na opiekuna",
     primaryEmail,
   });
 }
 
 /**
- * Handle form submit logic, returns values to be updated in sheet
- */
-function handleFormSubmit(
-  name: string,
-  surname: string,
-  primaryEmail: string,
-  superiorEmail: string
-) {
-  if (superiorEmail) {
-    notifySuperior(superiorEmail, name, surname);
-    return { status: "Oczekiwanie na opiekuna" };
-  }
-  sendEmail(
-    ADMIN_MAIL,
-    `Prośba o założenie konta ${primaryEmail}`,
-    `Zatwierdź: ${SpreadsheetApp.getActiveSpreadsheet().getUrl()}`
-  );
-  return { status: "Oczekiwanie na admina", isLeader: true };
-}
-
-/**
  * Send email with superior confirmation link
  */
-function notifySuperior(superiorEmail: string, name: string, surname: string) {
-  const id = proposeEmail(name, surname);
-  const mail = `${id}@zhr.pl`;
-  const verificationLink = `${PROXY_URL}/confirm-zhr.html?id=${id}`;
+function notifySuperior(
+  superiorEmail: string,
+  name: string,
+  surname: string,
+  mail: string
+) {
+  const verificationLink = `${PROXY_URL}/confirm-zhr.html?id=${mail}`;
   const template = HtmlService.createTemplateFromFile("superior");
   template.mail = mail;
   template.verificationLink = verificationLink;
