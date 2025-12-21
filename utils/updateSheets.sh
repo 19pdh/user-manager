bc5cb9df8f3da5c037760c74d98db08383c8c190
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-12-21 13:09:44 +0100

    revert: updateSheets

--- utils/updateSheets.sh
+++ utils/updateSheets.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+# On NixOS or with Nix installed run in:
+# nix-shell -p jq google-clasp
+#
 # USAGE:
-# ./utils/updateSheets.sh sheets.json [VERSION_TAG]
+# ./utils/updateSheets.sh sheets.json
 #
 # The sheets.json should be as provided in utils/example.sheets.json
 # Value of parentId is Google Sheet ID, and scriptId is script id
@@ -8,31 +11,21 @@
 # To learn more visit: https://github.com/google/clasp
 
 FILE="$1"
-TAG="$2"
-DATE=$(date +"%Y-%m-%d %H:%M")
-
-if [ -z "$FILE" ]; then
+[ -z "$FILE" ] &&
 	echo "USAGE: 
-  updateSheets.sh SHEETS_JSON_FILE [VERSION_TAG]
+  updateSheets.sh SHEETS_JSON_FILE
 
-  See utils/example.sheets.json"
+  See utils/example.sheets.json" &&
 	exit 1
-fi
 
-# Skip confirmation in CI environment
-if [ -z "$CI" ]; then
-    read -n 1 -r -s -p "Will deploy files from $PWD, continue?"
-    echo ""
-else
-    echo "Running in CI mode, skipping confirmation."
-fi
+read -n 1 -r -s -p "Will deploy files from $PWD, continue?"
+echo ""
 
 jq -c '.[]' "$FILE" | while read -r document; do
 	name=$(echo "$document" | jq -r .name)
 	parentId=$(echo "$document" | jq -r .parentId)
 	scriptId=$(echo "$document" | jq -r .scriptId)
 
-	echo "---------------------------------------------------"
 	echo "Updating $name"
 	cat <<EOF >.clasp.json
 {
@@ -41,33 +34,5 @@ jq -c '.[]' "$FILE" | while read -r document; do
   "parentId": $parentId
 }
 EOF
-
-    # Push code
-	echo "Pushing code..."
-	npx clasp push --force
-
-    # Handle deployments
-    echo "Handling deployments..."
-    if [ -n "$TAG" ]; then
-        DESCRIPTION="$TAG - $DATE"
-    else
-        DESCRIPTION="Manual deploy - $DATE"
-    fi
-
-    # List deployments and parse IDs
-    # Expected output format from clasp deployments:
-    # 2 Deployments.
-    # - <ID> @<Version> - <Description>
-    DEPLOYMENT_IDS=$(npx clasp deployments | grep "^- " | awk '{print $2}')
-
-    if [ -z "$DEPLOYMENT_IDS" ]; then
-        echo "No existing deployments found. Creating a new one..."
-        npx clasp deploy -d "$DESCRIPTION"
-    else
-        echo "Updating existing deployments..."
-        for id in $DEPLOYMENT_IDS; do
-            echo "Redeploying deployment ID: $id"
-            npx clasp deploy -i "$id" -d "$DESCRIPTION"
-        done
-    fi
+	yes | clasp push
 done
